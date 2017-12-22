ec93326c241cd97d5597732d7eee1b6ce26d2fbd
trevorjdobson <trevorjdobson@gmail.com>
2017-12-22 13:33:52 -0800

    solution found

---
+++ format-words-into-a-sentence/solution.js
@@ -0,0 +1,17 @@
+function formatWords(words){
+    for(var i = words.length - 1; i >= 0; i--) {
+      if(words[i] === '') {
+         words.splice(i, 1);
+      }
+  }
+    var newStr = "";
+      if (words.length === 1) {
+          newStr = words[0];
+      } else if (words.length === 2) {
+          newStr = words.join(' and ');
+      } else if (words.length > 2) {
+          newStr = words.slice(0, -1).join(', ') + ' and ' + words.slice(-1);
+      }
+      console.log(newStr)
+      return newStr;
+  }
